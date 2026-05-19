7fb29653cf30ce5e22828db66584f10622f4ae57
ShadowXu1130 <1299016085@qq.com>
2026-05-19 22:21:48 +0800

    Add initial pages and navbar structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
+import Navbar from "./components/Navbar"
+
 function App() {
   return (
     <div>
+      <Navbar />
+
       <h1>Entertainment Guild Web App</h1>
+
+      <p>Developed by Xinpeng Xu and Yuchi Qian</p>
     </div>
   )
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>Entertainment Guild</h2>
+    </nav>
+  )
+}
+
+export default Navbar
