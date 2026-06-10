997f9793986f285157d915bea56cd575e3b31537
ShadowXu1130 <1299016085@qq.com>
2026-06-10 14:21:53 +0800

    Connect products page to Entertainment Guild API

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,24 +1,34 @@
 import { useEffect, useState } from "react"
 
 function Products() {
+
   const [products, setProducts] = useState([])
 
   useEffect(() => {
-    fetch("http://localhost:5050/api/products")
+
+    fetch("http://localhost:3001/api/inft3050/Product")
       .then((response) => response.json())
-      .then((data) => setProducts(data))
+      .then((data) => {
+        setProducts(data.list)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
   }, [])
 
   return (
     <div>
+
       <h1>Products</h1>
 
       {products.map((product) => (
-        <div key={product.id}>
-          <h3>{product.name}</h3>
-          <p>${product.price}</p>
+        <div key={product.ID}>
+          <h3>{product.Name}</h3>
+          <p>{product.Author}</p>
         </div>
       ))}
+
     </div>
   )
 }
